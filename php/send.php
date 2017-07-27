4d032570644b6cc45496ae99126293cffd2baa33
Kate <kate@Kate-MacBook-Air.local>
2017-07-27 11:22:34 -0400

    contact form modeled after and working the same as what's in the out of the box template which I'm not certian works itself

--- php/send.php
+++ php/send.php
@@ -1,12 +1,17 @@
 <?php
 if(!$_POST) exit;
 
-
-  $to 	  = 'someemail@somedomain.com'; #Replace your email id...
-	$name	  = $_POST['txtname'];
-	$email    = $_POST['txtemail'];
-	$subject  = 'Support';
-    $comment  = $_POST['txtmessage'];
+if($location = 1){
+  $to = "desk@modafina.ca";
+}
+if($location = 2){
+  $to = "desk@hairfantasy.ca"
+}
+	$name = $_POST['name'];
+  $phone = $_POST['phone'];
+	$email    = $_POST['email'];
+	$subject  = 'An appointment reqest from your website';
+  $comment  = $_POST['msg'];
 
 	if(get_magic_quotes_gpc()) { $comment = stripslashes($comment); }
 
@@ -14,11 +19,12 @@ if(!$_POST) exit;
 
 	 $msg  = "You have been contacted by $name with regards to $subject.\r\n\n";
 	 $msg .= "$comment\r\n\n";
-	 $msg .= "You can contact $name via email, $email.\r\n\n";
+	 $msg .= "You can contact $name via email, $email , or via phone $phone.\r\n\n";
 	 $msg .= "-------------------------------------------------------------------------------------------\r\n";
 
 	 if(@mail($to, $e_subject, $msg, "From: $email\r\nReturn-Path: $email\r\n"))
 	 {
+     echo $msg;
 		 echo '<div class="dt-sc-success-box aligncenter"><i class="fa fa-check"></i><strong>Success!</strong>Thanks for <span> Contacting Us </span>, We will call back to you soon.</div> ';
 	 }
 	 else
